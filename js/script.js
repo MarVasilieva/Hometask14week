cffae07f86f6cfaf2fc1da85ce477f1434012ec5
MarVasilieva <marina_vasilieva@list.ru>
2022-12-08 23:01:34 +0600

    gallery does not work blin

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let photo = document.querySelectorAll('#gallery .photos img');
+let prev = document.querySelector('.prev');
+let next = document.querySelector('.next');
+let i = 0;
+prev.onclick = function(){
+    photo[i].style.display = 'none';
+    i--;
+    if (i < 0){
+     i = photo.length - 1;
+    }
+    photo[i].style.display = 'block'
+
+}
+next.onclick = function(){
+    photo[i].style.display = 'none';
+    i++;
+    if (i >= photo.length){
+     i = 0
+    }
+    photo[i].style.display = 'block'
+
+}
